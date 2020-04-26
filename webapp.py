d5a69f486d4f020b1a4f6449275bc366561b3199
kbrumer7 <62360804+kbrumer7@users.noreply.github.com>
2020-04-25 17:45:47 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
 
 from flask import Flask, render_template, request
 import app
+import json
+import requests
 app = Flask(__name__)
 
 global state
@@ -9,22 +11,34 @@ state = {'current_currency':"", 'current_amount':"", 'goal_currency':"", 'final_
 @app.route('/')
 @app.route('/main')
 def main():
-	return render_template('currencyconverter.html')
+    return render_template('currencyconverter.html')
 
 
 @app.route('/convert')
 def convert_currency():
-	global state
+    global state
+
+
+    return render_template('convert.html',state=state)
+
+
+@app.route('/results',methods=['GET','POST'])
+def results():
+    global state
     current_currency = request.form['current_currency']
     current_amount = request.form['current_amount']
     goal_currency = request.form['goal_currency']
+    state['current_currency'] = current_currency
+    state['current_amount'] = current_amount
+    state['goal_currency'] = goal_currency
 
-	return render_template('convert.html',state=state)
+    currency_exchange = requests.get('https://api.ratesapi.io/api/latest?base=USD')
+    currency_exchange = currency_exchange.text
+    currency_exchange = json.loads(currency_exchange)
+    """take out base USD and rates"""
+    currency_exchange = currency_exchange['rates']
 
-
-@app.route('/results')
-def results():
-    global state
+    print(currency_exchange)
 
 
     return render_template('results.html',state=state)
@@ -33,4 +47,4 @@ def results():
 
 
 if __name__ == '__main__':
-	app.run('0.0.0.0',port=3000)
+    app.run('0.0.0.0',port=3000)
